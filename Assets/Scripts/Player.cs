04dc2c6b29d81f8a70a6f17b383aacc18c85d2fa
Ebediam <felipebiosca@gmail.com>
2019-11-21 23:36:04 +0100

    Update #0
    Getting a base project ready to work, just player movement using Unity's new Input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    public PlayerInputs input;
+    public CharacterController controller;
+    public new Camera camera;
+
+
+    public float speed;
+
+
+    public Vector2 movementDirection;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        input = new PlayerInputs();
+        input.GamePlay.Jump.performed += Jump;
+
+        input.GamePlay.Move.performed += Move;
+        
+
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(new Vector3(-movementDirection.y, 0f, movementDirection.x)*speed*Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        movementDirection = context.ReadValue<Vector2>();
+
+    }
+
+    public void CameraMove(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        transform.localScale *= 1.1f;
+    }
+}
