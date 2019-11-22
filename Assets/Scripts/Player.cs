8972d7647bf60088ea4ea0d4825530541d642368
Ebediam <felipebiosca@gmail.com>
2019-11-22 08:25:45 +0100

    Update #0b
    -Added a Cinemachine camera
    
    -Added jumping with adjustable jump height depending on the amount of time you have the jump button pressed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,38 @@ public class Player : MonoBehaviour
 {
     public PlayerInputs input;
     public CharacterController controller;
-    public new Camera camera;
 
+    public Transform groundCheck;
+    public float groundCheckDistance;
+
+    public LayerMask groundLayer;
+
+    bool isGrounded;
+    bool jumpBoost;
 
     public float speed;
 
+    public float gravity = -9.81f;
 
-    public Vector2 movementDirection;
+    Vector3 velocity;
+
+    public float jumpHeight;
+    public float jumpBoostAmount;
+    public float jumpBoostMaxTime;
+    float jumpVelocity;
+
+
+    Vector2 movementDirection;
     // Start is called before the first frame update
     void Awake()
     {
         input = new PlayerInputs();
         input.GamePlay.Jump.performed += Jump;
+        input.GamePlay.Jump.canceled += JumpBoostEnds;
 
         input.GamePlay.Move.performed += Move;
+
+        jumpVelocity = Mathf.Sqrt(jumpHeight*gravity*-2f);
         
 
     }
@@ -36,7 +54,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundLayer);
+                     
+        if(isGrounded)
+        {
+            jumpBoost = true;
+            if(velocity.y < 0f)
+            {
+                velocity.y = -2f;
+            }
+        }
+        else
+        {
+            if (jumpBoost)
+            {
+                velocity.y += jumpBoostAmount * Time.deltaTime;
+            }
+        }
+
         controller.Move(new Vector3(-movementDirection.y, 0f, movementDirection.x)*speed*Time.deltaTime);
+
+
+        velocity.y += gravity*Time.deltaTime;
+
+        controller.Move(velocity*Time.deltaTime);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -52,6 +93,27 @@ public class Player : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        transform.localScale *= 1.1f;
+        if (!isGrounded)
+        {
+            return;
+        }
+
+        
+        velocity.y = jumpVelocity;
+        jumpBoost = true;
+        Invoke("JumpBoostEndInvoke", jumpBoostMaxTime);
+
+
+    }
+
+    public void JumpBoostEnds(InputAction.CallbackContext context)
+    {
+        JumpBoostEndInvoke();
+    }
+
+    public void JumpBoostEndInvoke()
+    {
+        CancelInvoke("JumpBoostEndInvoke");
+        jumpBoost = false;
     }
 }
